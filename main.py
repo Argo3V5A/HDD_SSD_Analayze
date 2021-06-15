566feda3ebeb071ca708a66fd32f72f8acb27731
Arsen Gumin <gumin@live.ru>
2021-06-16 00:06:26 +0300

    'stable'

---
+++ main.py
@@ -0,0 +1,34 @@
+import warnings
+import json
+from itertools import chain
+from src.imdict import imdict
+from src.etl.extract import (collect_product_pages,
+                             get_product_amount_page,
+                             generate_headers,
+                             collect_url_from_page,
+                             get_product_data,
+                             get_page_html)
+
+if __name__ == '__main__':
+
+    warnings.filterwarnings('ignore', U)
+
+    data = imdict({'URL': 'https://www.e-katalog.ru/list/61/',
+                   'DOMAIN': 'https://www.e-katalog.ru'})
+
+    headers = imdict({'browser': "chrome",
+                      'os': "win",
+                      'headers': True})
+
+    pages_urls = collect_product_pages(data['URL'], get_product_amount_page(data['URL'], generate_headers(headers)))
+
+    all_url_product = list(chain(*list(map(collect_url_from_page, pages_urls,[generate_headers(headers)]))))  # Распоковка всех списков
+
+    full_url_path_to_product = list(map(lambda x: data['DOMAIN'] + x, all_url_product))
+
+    with open('ssd_data.json', mode='a', encoding='UTF8') as f:
+        for url in full_url_path_to_product:
+            temp = get_product_data(get_page_html(url))
+            json.dump(temp, f, indent=4, ensure_ascii=False)
+            temp.clear()
+        f.close()
---
+++ src/imdict.py
@@ -0,0 +1,18 @@
+
+class imdict(dict):
+
+    '''Неизменный словарь. Источник: оригинальная документация'''
+
+    def __hash__(self):
+        return id(self)
+
+    def _immutable(self, *args, **kws):
+        raise TypeError('object is immutable')
+
+    __setitem__ = _immutable
+    __delitem__ = _immutable
+    clear       = _immutable
+    update      = _immutable
+    setdefault  = _immutable
+    pop         = _immutable
+    popitem     = _immutable
